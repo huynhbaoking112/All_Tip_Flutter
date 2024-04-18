409cd56dd7b23a89b8365a0935258b971a1bd24f
BaoKing <huynhbaoking@gmail.com>
2024-04-18 20:52:48 +0700

    extension_clean_code

---
+++ tip_extension_clean_code/android/app/src/main/kotlin/com/example/tip_extension_clean_code/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tip_extension_clean_code
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ tip_extension_clean_code/lib/customExtension/custom.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:flutter/material.dart';
+
+extension customWidget on Widget {
+
+
+  Widget center(){
+    return Center(
+      child: this,
+    );
+  }
+
+
+}
